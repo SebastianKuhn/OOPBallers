bf43f86515fdb0a70e9d76f845852f14a63a65bc
Thomas Meichtry <thomasmeichtry@eduroamstud-130-82-246-190.unisg.ch>
2018-05-01 21:27:30 +0200

    outlined main method

---
+++ main.py
@@ -0,0 +1,14 @@
+#import classes
+from generatejsonrequest import GenerateGoogleVisionRequest
+
+
+#This script ist contorlling the programm.
+def start_program():
+    isFinished = False
+
+    while isFinished:
+        print("Please choose your picture, which you want to analyze.")
+
+#the main function is the first function to be executed when starting a programm.
+if __name__ == "__main__":
+    start_program()
