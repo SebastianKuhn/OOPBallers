f488a87dfac09da98f26a4f5b509cea78d3e7f00
Thomas Meichtry <thomasmeichtry@eduroamstud-130-82-246-190.unisg.ch>
2018-05-02 11:43:29 +0200

    GeneratGoogleVisionRequest ist ungünstig. Neuer Versuch: API Call mit HTTP Post Request.

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 #import classes
-from generatejsonrequest import GenerateGoogleVisionRequest
+import base64
+import urllib
+import json
 
+#image path of picture
+base_path_image = "/Users/thomasmeichtry/Desktop/Application based Object-oriented Programming/" \
+                  "OOPBallers/Pictures/Karotten.jpg"
 
-#This script ist contorlling the programm.
-def start_program():
-    isFinished = False
+def send_request():
+    """ sends a request to the Google Vision API """
 
-    while isFinished:
-        print("Please choose your picture, which you want to analyze.")
+    base_url = "https://vision.googleapis.com/v1/images:annotate?key="
+    api_key = "AIzaSyDfKIKpRF7OBu5Q3Q6Zh1zxFOsulyTZ2IE"
+    url = base_url + api_key
+
+    json_request = {
+                      "requests":[
+                        {
+                          "image":{
+                            #encoded_image
+                          },
+                          "features":[
+                            {
+                              "type":"LABEL_DETECTION",
+                              "maxResults":1
+                            }
+                          ]
+                        }
+                      ]
+                    }
+
+def encode_image_as_base64(image_path):
+    """ Loads an image from path and returns it as base64 encoded string """
+
+    print("Encoding image: "+ image_path)
+
+    encoded_string = ""
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+
+    return encoded_string
+
 
 #the main function is the first function to be executed when starting a programm.
 if __name__ == "__main__":
-    start_program()
+    print(encode_image_as_base64(base_path_image))
