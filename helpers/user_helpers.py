a666b2e987f8ec3c639a5f8860b9f55c8d94559c
JLeop <julian@rega-sense.ch>
2018-05-17 10:22:54 +0200

    users helpers function, getpw still does not work

--- helpers/user_helpers.py
+++ helpers/user_helpers.py
@@ -8,7 +8,7 @@ import db_helpers as helpers
 def newuser(userid, name, password, vegetarian):
     db = helpers.getDbCon()
     cursor = db.cursor()
-    hashedpw = password
+    hashedpw = password #not being hashed yet
     userInsertQuery = "INSERT into users (user_id, username, password, vegetarian) VALUES (%s, %s, %s, %s)"
     # try:
     cursor.execute(userInsertQuery, (userid, name, hashedpw, vegetarian)) # to replace s% put in quotation markes
@@ -17,22 +17,27 @@ def newuser(userid, name, password, vegetarian):
 def getAllUsers():
     db = helpers.getDbCon()
     cursor = db.cursor()
-
     getUsersQuery = "SELECT * FROM users"
     cursor.execute(getUsersQuery)
-    # print(cursor.fetchall())
+    print(cursor.fetchall())
     return cursor.fetchall()
 
-
-def login(record):
-    db = getDbCon()
+#function does not work. asked Ruben
+def getPassword(user_name):
+    db = helpers.getDbCon()
     cursor = db.cursor()
-
-    kpiInsertQuery = "INSERT into kpis (song_id, name, value, crawl_time) VALUES (%s, %s, %s, %s)"
+    getpassword = "SELECT password FROM users WHERE username = %s"
     # try:
-    cursor.execute(kpiInsertQuery, (record["song_id"], record["name"], record["value"], record["crawl_time"]))
-    db.commit()
+    cursor.execute(getpassword, user_name)
+    print(cursor.fetchone())
+    return cursor.fetchone()
+
+
+
+
+# ------------ working functions --------------------------------------------
 
 
 #newuser(1, "Julian", "newpassword", 0)
-getAllUsers()
+#newuser(3, "Sebastian", "supersecurepassword", 1)
+#getAllUsers()
