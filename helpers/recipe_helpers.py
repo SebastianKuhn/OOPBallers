e5d09ad3893aa4111f630feca895689fa453a4dc
SebastianKuhn <sebastian.kuhn@bluewin.ch>
2018-05-18 16:36:17 +0200

    some changes to recipe_helpers

--- helpers/recipe_helpers.py
+++ helpers/recipe_helpers.py
@@ -5,8 +5,25 @@ import db_helpers as helpers
 def newRecipe(re_id, re_title, re_readyinmin, re_servings, re_vegi, re_url, re_aggr, re_health_sc):
     db = helpers.getDbCon()
     cursor = db.cursor()
-    recipeInsertQuery = "INSERT into recipes (recipe_id, title, ready_in_minutes, servings, vegetarian, source_url, aggregate_likes, health_score) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    # try:
-    cursor.execute(recipeInsertQuery, (re_id, re_title, re_readyinmin, re_servings, re_vegi, re_url, re_aggr, re_health_sc))
-    db.commit()
-    print("Successfully added the recipe " + re_title)
+    recipeInsertQuery = "INSERT IGNORE into recipes (recipe_id, title, ready_in_minutes, servings, vegetarian, source_url, aggregate_likes, health_score)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    try:
+        cursor.execute(recipeInsertQuery, (re_id, re_title, re_readyinmin, re_servings, re_vegi, re_url, re_aggr, re_health_sc))
+        db.commit()
+    except Exception:
+        return "OOPs"
+    finally:
+        cursor.close()
+        db.close()
+
+def addRecipetoUser()
+
+    try:
+        cursor.execute(userInsertQuery, (name, hashedpw, vegetarian))  # to replace s% put in quotation markes
+        db.commit()
+    except Exception:
+        return 'Error: unable to execute!'
+
+    finally:
+        cursor.close()
+        db.close()
