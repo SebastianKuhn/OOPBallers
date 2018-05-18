1c4f225d910d5e281fa4199dbe1bc73ef665510d
JLeop <julian@rega-sense.ch>
2018-05-18 13:30:40 +0200

    user helpers

--- helpers/user_helpers.py
+++ helpers/user_helpers.py
@@ -1,30 +1,12 @@
-import hashlib
-import MySQLdb # pip install mysql-client or something else
-# Add new song into songs table
-# used by chart crawlers
-from configparser import ConfigParser
-<<<<<<< HEAD
-import db_helpers as helpers
+db_helpers as helpers
 from contextlib import closing
 
-def newUser(name, password, vegetarian):
+def newUser(name, pw, vegetarian):
     db = helpers.getDbCon()
     with closing(db.cursor()) as cursor:
-        hashedpw = password #not being hashed yet
         userInsertQuery = "INSERT into users (username, password, vegetarian) VALUES (%s, %s, %s)"
         # try:
-        cursor.execute(userInsertQuery, (name, hashedpw, vegetarian)) # to replace s% put in quotation markes
-=======
-import helpers.db_helpers as helpers
-
-
-def newUser(name, hashed_password, vegetarian):
-    db = helpers.getDbCon()
-    cursor = db.cursor()
-    userInsertQuery = "INSERT into users (username, password, vegetarian) VALUES (%s, %s, %s)"
-    # try:
-    cursor.execute(userInsertQuery, (name, hashed_password, vegetarian)) # to replace s% put in quotation markes
->>>>>>> 022cd3426a6a23477b20cfeb9fc03910aafcab35
+        cursor.execute(userInsertQuery, (name, pw, vegetarian)) # to replace s% put in quotation markes
     db.commit()
     print("Successfully added " + name)
 
@@ -68,19 +50,4 @@ def deleteUser(name):
     #try
     cursor.execute(deleteuser, (name,))
     print("You deleted the user: " + name)
-<<<<<<< HEAD
     db.commit()
-
-
-
-
-
-# ------------ working functions --------------------------------------------
-
-newUser("Max", "124", 0)
-#newuser(3, "Sebastian", "supersecurepassword", 1)
-#getAllUsers()
-#deleteUser("Sinan")
-=======
-    db.commit()
->>>>>>> 022cd3426a6a23477b20cfeb9fc03910aafcab35
