48b745e93d968f03c021dce56d12ddab4d08712b
JLeop <julian@rega-sense.ch>
2018-05-18 16:11:21 +0200

    helpers

--- helpers/db_helpers.py
+++ helpers/db_helpers.py
@@ -6,12 +6,16 @@ def getDbCon():
     parser = ConfigParser()
     try:
         parser.read('config/config.ini')
+        db_host = parser.get('db', 'host')
+        db_user = parser.get('db', 'user')
+        db_password = parser.get('db', 'password')
+        db_name = parser.get('db', 'db')
     except Exception:
         parser.read('../config/config.ini')
-    db_host = parser.get('db','host')
-    db_user = parser.get('db','user')
-    db_password = parser.get('db','password')
-    db_name = parser.get('db','db')
+        db_host = parser.get('db','host')
+        db_user = parser.get('db','user')
+        db_password = parser.get('db','password')
+        db_name = parser.get('db','db')
     print(db_name)
 
     return (MySQLdb.connect(db_host, db_user, db_password, db_name))
