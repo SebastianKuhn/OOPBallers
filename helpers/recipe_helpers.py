9dfed4b0ee367ba9e58bfc8125ebf71bc2bbffe2
SebastianKuhn <sebastian.kuhn@bluewin.ch>
2018-05-19 00:13:28 +0200

    added functionality to recipe

--- helpers/recipe_helpers.py
+++ helpers/recipe_helpers.py
@@ -18,25 +18,53 @@ def newRecipe(recipe):
         db.close()
 
 
-def checkifUserRecipeAlreadyExists(user_id, recipe_id):
+def checkifUserRecipeAlreadyExists(user, recipe):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     userRecipeCheckQuery = "SELECT * FROM user_recipes WHERE user_id = %s and recipe_id = %s;"
     try:
-        cursor.execute(userRecipeCheckQuery, (user_id, recipe_id))  # to replace s% put in quotation marks
+        cursor.execute(userRecipeCheckQuery, (user.user_id, recipe.recipe_id))  # to replace s% put in quotation marks
         result = cursor.fetchall()
         return result
     except Exception:
         return "Error: OOPs something went wrong!"
 
-def addRecipetoUser(user_id, recipe_id):
+def addRecipetoUser(user, recipe):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     userRecipeInsertQuery = """INSERT into user_recipes (user_id, recipe_id) VALUES (%s, %s)"""
-    check = checkifUserRecipeAlreadyExists(user_id, recipe_id)
+    check = checkifUserRecipeAlreadyExists(user.user_id, recipe.recipe_id)
     if check == ():
         try:
-            cursor.execute(userRecipeInsertQuery, (user_id, recipe_id))  # to replace s% put in quotation markes
+            cursor.execute(userRecipeInsertQuery, (user.user_id, recipe.recipe_id))  # to replace s% put in quotation markes
+            db.commit()
+        except Exception:
+            return 'Error: unable to execute!'
+        finally:
+            cursor.close()
+            db.close()
+    else:
+        pass
+
+def checkifRecipeStepAlreadyExists(recipe, instruction)):
+    db = db_helpers.getDbCon()
+    cursor = db.cursor()
+    userRecipeStepCheckQuery = "SELECT * FROM recipe_step WHERE recipe_id = %s and number = %s and step = %s;"
+    try:
+        cursor.execute(userRecipeStepCheckQuery, (recipe.recipe_id, instruction.number, instruction.step))  # to replace s% put in quotation marks
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        return "Error: OOPs something went wrong!"
+
+def addRecipeInstructionText(recipe, instruction):
+    db = db_helpers.getDbCon()
+    cursor = db.cursor()
+    userRecipeInsertQuery = """INSERT into recipe_step (recipe_id, number, step)  VALUES (%s, %s, %s)"""
+    check = checkifUserRecipeAlreadyExists(recipe.recipe_id, instruction.number, instruction.step)
+    if check == ():
+        try:
+            cursor.execute(userRecipeInsertQuery, (user.user_id, recipe.recipe_id))  # to replace s% put in quotation markes
             db.commit()
         except Exception:
             return 'Error: unable to execute!'
