4eedfa5bd8691a353f8fbd52c8d6bbc4e6e497d3
SebastianKuhn <sebastian.kuhn@bluewin.ch>
2018-05-19 01:37:09 +0200

    changes to equipment and ingredient helpers

--- helpers/equipment_helpers.py
+++ helpers/equipment_helpers.py
@@ -1,12 +1,12 @@
 from helpers import db_helpers
 
 
-def newEquipment(Equipment):
+def newEquipment(equipment):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     equipmentInsertQuery = "INSERT IGNORE into recipes (equipment) VALUES (%s);"
     try:
-        cursor.execute(equipmentInsertQuery, (Equipment.name))
+        cursor.execute(equipmentInsertQuery, (equipment.name))
         db.commit()
     except Exception:
         return "OOPs"
@@ -15,25 +15,25 @@ def newEquipment(Equipment):
         db.close()
 
 
-def checkifUserEquipmentAlreadyExists(user_id, equipment_id):
+def checkifUserEquipmentAlreadyExists(user, equipment):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     userRecipeCheckQuery = "SELECT * FROM user_equipment WHERE user_id = %s and equipment_id = %s;"
     try:
-        cursor.execute(userRecipeCheckQuery, (user_id, equipment_id))  # to replace s% put in quotation marks
+        cursor.execute(userRecipeCheckQuery, (user.user_id, equipment.equipment_id))  # to replace s% put in quotation marks
         result = cursor.fetchall()
         return result
     except Exception:
         return "Error: OOPs something went wrong!"
 
-def addEquipmenttoUser(user_id, equipment_id):
+def addEquipmenttoUser(user, equipment):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     userEquipmentInsertQuery = """INSERT into user_equipment (user_id, equipment_id) VALUES (%s, %s)"""
-    check = checkifUserEquipmentAlreadyExists(user_id, equipment_id)
+    check = checkifUserEquipmentAlreadyExists(user.user_id, equipment.equipment_id)
     if check == ():
         try:
-            cursor.execute(userEquipmentInsertQuery, (user_id, equipment_id))  # to replace s% put in quotation markes
+            cursor.execute(userEquipmentInsertQuery, (user.user_id, equipment.equipment_id))  # to replace s% put in quotation markes
             db.commit()
         except Exception:
             return 'Error: unable to execute!'
@@ -43,25 +43,25 @@ def addEquipmenttoUser(user_id, equipment_id):
     else:
         pass
 
-def checkifRecipeEquipmentAlreadyExists(recipe_id, equipment_id):
+def checkifRecipeEquipmentAlreadyExists(recipe, equipment):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     recipeEquipmentCheckQuery = "SELECT * FROM recipe_equipment WHERE recipe_id = %s and equipment_id = %s;"
     try:
-        cursor.execute(recipeEquipmentCheckQuery, (recipe_id, equipment_id))  # to replace s% put in quotation marks
+        cursor.execute(recipeEquipmentCheckQuery, (recipe.recipe_id, equipment.equipment_id))  # to replace s% put in quotation marks
         result = cursor.fetchall()
         return result
     except Exception:
         return "Error: OOPs something went wrong!"
 
-def addEquipmenttoRecipe(recipe_id, equipment_id):
+def addEquipmenttoRecipe(recipe, equipment):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     recipeEquipmentInsertQuery = """INSERT into recipe_equipment (recip_id, equipment_id) VALUES (%s, %s)"""
-    check = checkifRecipeEquipmentAlreadyExists(recipe_id, equipment_id)
+    check = checkifRecipeEquipmentAlreadyExists(recipe.recipe_id, equipment.equipment_id)
     if check == ():
         try:
-            cursor.execute(recipeEquipmentInsertQuery, (recipe_id, equipment_id))  # to replace s% put in quotation markes
+            cursor.execute(recipeEquipmentInsertQuery, (recipe.recipe_id, equipment.equipment_id))  # to replace s% put in quotation markes
             db.commit()
         except Exception:
             return 'Error: unable to execute!'
@@ -72,9 +72,9 @@ def addEquipmenttoRecipe(recipe_id, equipment_id):
         pass
 
 def getEquipmentId(equipment):
-    db = helpers.getDbCon()
+    db = db_helpers.getDbCon()
     cursor = db.cursor()
     equipmentIdQuery = "SELECT equipment_id FROM equipments WHERE username = %s"
-    cursor.execute(userIdQuery, (equipment,))
+    cursor.execute(equipmentIdQuery, (equipment.name,))
     equipment_id = cursor.fetchone()
     return equipment_id
--- helpers/ingredient_helpers.py
+++ helpers/ingredient_helpers.py
@@ -20,39 +20,55 @@ def newIngredient(ingredient):
 
 # for recipe_ingredients table
 # the number is the step in in which the ingredient is used
-def addIngredientToRecipe(ingredient, recipe, instruction):
+def checkifRecipeIngredientAlreadyExists(recipe, ingredient, instruction):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
-    ingredientInsertQuery = "INSERT into ingredients (recipe_id, number, ingredient_id, amount, unit) VALUES (%s, %s, %s, %s, %s);"
+    recipeIngredientCheckQuery = """SELECT * FROM recipe_ingredients 
+                          WHERE recipe_id = %s and number = %s and ingredient_id = %s and amount = %s and unit = %s;"""
     try:
-        cursor.execute(ingredientInsertQuery, (recipe.recipe_id, instruction.number, ingredient.ingredient_id, ingredient.amount, ingredient.unit))
-        db.commit()
-    except Exception:
-        return "OOPs something went wrong"
-    finally:
-        cursor.close()
-        db.close()
-
-# for user_ingredients table
-def checkifUserIngredientAlreadyExists(user_id, ingredient_id):
-    db = db_helpers.getDbCon()
-    cursor = db.cursor()
-    userIngredientCheckQuery = "SELECT * FROM user_ingredients WHERE user_id = %s and ingredient_id = %s;"
-    try:
-        cursor.execute(userIngredientCheckQuery, (user_id, ingredient_id))  # to replace s% put in quotation marks
+        cursor.execute(recipeIngredientCheckQuery, (recipe.recipe_id, instruction.number, ingredient.ingredient_id, ingredient.amount, ingredient.unit))  # to replace s% put in quotation marks
         result = cursor.fetchall()
         return result
     except Exception:
         return "Error: OOPs something went wrong!"
 
-def addIngredienttoUser(user_id, ingredient_id):
+def addIngredienttoRecipe(recipe, ingredient, instruction):
+    db = db_helpers.getDbCon()
+    cursor = db.cursor()
+    recipeIngredientInsertQuery = """INSERT into recipe_ingredients (recipe_id, number, ingredient_id, amount, unit) VALUES (%s, %s, %s, %s, %s)"""
+    check = checkifRecipeIngredientAlreadyExists(recipe, ingredient, instruction)
+    if check == ():
+        try:
+            cursor.execute(recipeIngredientInsertQuery, (recipe.recipe_id, instruction.number, ingredient.ingredient_id, ingredient.amount, ingredient.unit))  # to replace s% put in quotation markes
+            db.commit()
+        except Exception:
+            return 'Error: unable to execute!'
+        finally:
+            cursor.close()
+            db.close()
+    else:
+        pass
+
+# for user_ingredients table
+def checkifUserIngredientAlreadyExists(user, ingredient):
+    db = db_helpers.getDbCon()
+    cursor = db.cursor()
+    userIngredientCheckQuery = "SELECT * FROM user_ingredients WHERE user_id = %s and ingredient_id = %s;"
+    try:
+        cursor.execute(userIngredientCheckQuery, (user.user_id, ingredient.ingredient_id))  # to replace s% put in quotation marks
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        return "Error: OOPs something went wrong!"
+
+def addIngredienttoUser(user, ingredient):
     db = db_helpers.getDbCon()
     cursor = db.cursor()
     userIngredientInsertQuery = """INSERT into user_recipes (user_id, ingredient_id) VALUES (%s, %s)"""
-    check = checkifIngredientAlreadyExists(user_id, ingredient_id)
+    check = checkifUserIngredientAlreadyExists(user.user_id, ingredient.ingredient_id)
     if check == ():
         try:
-            cursor.execute(userIngredientInsertQuery, (user_id, ingredient_id))  # to replace s% put in quotation markes
+            cursor.execute(userIngredientInsertQuery, (user.user_id, ingredient.ingredient_id))  # to replace s% put in quotation markes
             db.commit()
         except Exception:
             return 'Error: unable to execute!'
