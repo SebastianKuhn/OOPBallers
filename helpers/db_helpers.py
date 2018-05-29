f2438eb476e4cbc04a4c9907ecb2290be6ce657a
SebastianKuhn <sebastian.kuhn@bluewin.ch>
2018-05-29 23:30:06 +0200

    notes

--- helpers/db_helpers.py
+++ helpers/db_helpers.py
@@ -19,9 +19,9 @@ def getDbCon():
         db_name = parser.get('db', 'db')
     except Exception:
         parser.read('../config/config.ini')
-        db_host = parser.get('db','host')
-        db_user = parser.get('db','user')
-        db_password = parser.get('db','password')
-        db_name = parser.get('db','db')
+        db_host = parser.get('db', 'host')
+        db_user = parser.get('db', 'user')
+        db_password = parser.get('db', 'password')
+        db_name = parser.get('db', 'db')
 
-    return (MySQLdb.connect(db_host, db_user, db_password, db_name, use_unicode=True, charset="utf8"))
+    return MySQLdb.connect(db_host, db_user, db_password, db_name, use_unicode=True, charset="utf8")
